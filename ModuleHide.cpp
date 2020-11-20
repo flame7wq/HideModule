964f2b2bef6c0a0624242713bb2d44e2f2717d45
flame7wq <54255036+flame7wq@users.noreply.github.com>
2020-11-20 21:17:22 +0800

    Update ModuleHide.cpp

--- ModuleHide.cpp
+++ ModuleHide.cpp
@@ -77,15 +77,19 @@ int main()
     HideModule("kernel32.dll");
     printf("******已成功隐藏模块 Kernel32.dll *******\n");
     getchar();
+    MODULEENTRY32 me32 = { sizeof(MODULEENTRY32) };
+    HANDLE hModuleSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, 0);
+    if (hModuleSnap == INVALID_HANDLE_VALUE)
+        return -1;
+
+    if (Module32First(hModuleSnap, &me32))
+    {
+        do
+        {
+            wprintf(TEXT("%s\n"), me32.szExePath);
+            printf("    Module address in the process: %p\n", me32.hModule);
+        } while (Module32Next(hModuleSnap,&me32));
+    }
+    CloseHandle(hModuleSnap);
+    return 1;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
